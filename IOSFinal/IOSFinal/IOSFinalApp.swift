db18705dd0312eb61ca263ad12c666f75f49b292
CrazyFatBoi2005 <spa.ez@mail.ru>
2025-12-25 00:24:36 +0500

    init commit

---
+++ IOSFinal/IOSFinal/IOSFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSFinalApp.swift
+//  IOSFinal
+//
+//  Created by Vassilissa on 25.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct IOSFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
