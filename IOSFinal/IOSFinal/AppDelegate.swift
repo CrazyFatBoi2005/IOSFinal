7973a7d09477b1e13882de16f11c0ac7aa602e3b
crazy <spa.ez@mail.ru>
2025-12-25 03:06:52 +0600

    bs fix

--- IOSFinal/IOSFinal/AppDelegate.swift
+++ IOSFinal/IOSFinal/AppDelegate.swift
@@ -7,13 +7,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = MainTabBarController()
+        window?.makeKeyAndVisible()
         return true
-    }
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
 }
